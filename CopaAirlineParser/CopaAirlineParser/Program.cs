0c38d5b2e41756c26e7fac49a5ebd04a7b5c3e91
mvanlaar <martijn@mvanlaar.net>
2015-07-10 09:10:23 +0200

    only need to fix flightduration

--- CopaAirlineParser/CopaAirlineParser/Program.cs
+++ CopaAirlineParser/CopaAirlineParser/Program.cs
@@ -10,13 +10,42 @@ using ICSharpCode.SharpZipLib.Zip;
 using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
 using ICSharpCode.SharpZipLib.Tar;
 using ICSharpCode.SharpZipLib.GZip;
-using ICSharpCode.SharpZipLib.Core; 
-
+using ICSharpCode.SharpZipLib.Core;
+using System.Globalization;
 
 namespace CopaAirlineParser
 {
-    class Program
+    public class Program 
     {
+        [Serializable]
+        public class CIFLight
+        {
+            // Auto-implemented properties. 
+
+            public string FromIATA;
+            public string ToIATA;
+            public DateTime FromDate;
+            public DateTime ToDate;
+            public Boolean FlightMonday;
+            public Boolean FlightTuesday;
+            public Boolean FlightWednesday;
+            public Boolean FlightThursday;
+            public Boolean FlightFriday;
+            public Boolean FlightSaterday;
+            public Boolean FlightSunday;
+            public DateTime DepartTime;
+            public DateTime ArrivalTime;
+            public String FlightNumber;
+            public String FlightAirline;
+            public String FlightOperator;
+            public String FlightAircraft;
+            public Boolean FlightCodeShare;
+            public Boolean FlightNextDayArrival;
+            public int FlightNextDays;
+            public string FlightDuration;
+        }
+        
+        
         static void Main(string[] args)
         {
             // Please install http://desktop.innoflightmaps.com/download/cm/cm-EN/index.html
@@ -28,6 +57,13 @@ namespace CopaAirlineParser
             string sqldb = Path.Combine(myDir, "cm.sqlite");
             string dbdownload = Path.Combine(myDir, "cm.gz");
             string filePath = Path.Combine(appDataFolder, "CMsafdasdf.db");
+            DateTime ValidFrom = DateTime.MinValue;
+            DateTime ValidTo = DateTime.MinValue;
+            List<CIFLight> CIFLights = new List<CIFLight> { };
+            CultureInfo ci = new CultureInfo("en-US");
+            string dateformat = "MM/dd/yyyy h:mm:ss tt";
+
+
             if (File.Exists(filePath)) {                
                 File.Copy(filePath,Path.Combine(myDir,"cm.sqlite"));               
             }
@@ -92,13 +128,113 @@ namespace CopaAirlineParser
             SQLiteConnection m_dbConnection;
             m_dbConnection = new SQLiteConnection("Data Source=" + sqldb + ";Version=3;");
             m_dbConnection.Open();
-            string sql = "select * from flights inner join legs on flights.flightId = legs.flightId where flights.numLegs=1";
+            string sqlupdate = "select * from lastupdated;";
+            string sql = "select flights.dptSta, flights.arvSta, datetime(effDate) as effDate, datetime(discDate) as discDate, opMon, opTue, opWed, opThu, opFri, opSat, opSun, cast(dptTimeLocal as TEXT) as dptTimeLocal,  cast(arvTimeLocal as TEXT) as arvTimeLocal, carrier, flightNum, equipCode, codeshare, opCarrier, arvNextDay, legNextDay from flights inner join legs on flights.flightId = legs.flightId where flights.numLegs=1 and legs.numStops =0";
+            SQLiteCommand commandupd = new SQLiteCommand(sqlupdate, m_dbConnection);
+            SQLiteDataReader readerupd = commandupd.ExecuteReader();
+            while (readerupd.Read())
+            {
+                ValidFrom = DateTime.Parse(readerupd["UtgValidityStart"].ToString());
+                ValidTo = DateTime.Parse(readerupd["UtgValidityEnd"].ToString());
+            }
+
             SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
             SQLiteDataReader reader = command.ExecuteReader();
             while (reader.Read())
             {
-                Console.WriteLine("ID: " + reader["flightId"] + "\tFrom: " + reader["dptCity"] + "\tTo: " + reader["arvCity"]);
+                string TEMP_FromIATA = null;
+                string TEMP_ToIATA = null;
+                DateTime TEMP_ValidFrom = new DateTime();
+                DateTime TEMP_ValidTo = new DateTime();                
+                Boolean TEMP_FlightMonday = false;
+                Boolean TEMP_FlightTuesday = false;
+                Boolean TEMP_FlightWednesday = false;
+                Boolean TEMP_FlightThursday = false;
+                Boolean TEMP_FlightFriday = false;
+                Boolean TEMP_FlightSaterday = false;
+                Boolean TEMP_FlightSunday = false;
+                DateTime TEMP_DepartTime = new DateTime();
+                DateTime TEMP_ArrivalTime = new DateTime();
+                Boolean TEMP_FlightCodeShare = false;
+                string TEMP_FlightNumber = null;
+                string TEMP_Aircraftcode = null;
+                TimeSpan TEMP_DurationTime = TimeSpan.MinValue;
+                Boolean TEMP_FlightNextDayArrival = false;
+                int TEMP_FlightNextDays = 0;
+                string TEMP_FlightOperator = null;
+                string TEMP_Airline = null;
+
+                TEMP_FromIATA = reader["dptSta"].ToString();
+                TEMP_ToIATA = reader["arvSta"].ToString();
+                TEMP_ValidFrom = DateTime.Parse(reader["effDate"].ToString());
+                TEMP_ValidTo = DateTime.Parse(reader["discDate"].ToString());
+                TEMP_FlightMonday = Boolean.Parse(reader["opMon"].ToString());
+                TEMP_FlightTuesday = Boolean.Parse(reader["opTue"].ToString());
+                TEMP_FlightWednesday = Boolean.Parse(reader["opWed"].ToString());
+                TEMP_FlightThursday = Boolean.Parse(reader["opThu"].ToString());
+                TEMP_FlightFriday = Boolean.Parse(reader["opFri"].ToString());
+                TEMP_FlightSaterday = Boolean.Parse(reader["opSat"].ToString());
+                TEMP_FlightSunday = Boolean.Parse(reader["opSun"].ToString());
+                string tempfrom = reader["dptTimeLocal"].ToString();
+
+                TEMP_DepartTime = DateTime.ParseExact(reader["dptTimeLocal"].ToString(), dateformat, ci);
+                TEMP_ArrivalTime = DateTime.ParseExact(reader["arvTimeLocal"].ToString(), dateformat, ci);
+                TEMP_FlightNumber = reader["carrier"].ToString() + reader["flightNum"].ToString();
+                TEMP_Aircraftcode = reader["equipCode"].ToString();
+                if (reader["codeshare"].ToString() == "1")
+                {
+                    TEMP_FlightOperator = reader["opCarrier"].ToString();
+                    TEMP_FlightCodeShare = true;
+                }
+                else
+                {
+                    TEMP_FlightOperator = reader["carrier"].ToString();
+                    TEMP_FlightCodeShare = false;
+                }
+                if (reader["arvNextDay"].ToString() == "1")
+                {
+                    TEMP_FlightNextDayArrival = true;
+                }                
+                TEMP_Airline = reader["carrier"].ToString();
+                TEMP_FlightNextDays = int.Parse(reader["legNextDay"].ToString());
+                TEMP_DurationTime = TimeSpan.FromMinutes(double.Parse(reader["legNextDay"].ToString()));
+
+                CIFLights.Add(new CIFLight
+                {
+                    FromIATA = TEMP_FromIATA,
+                    ToIATA = TEMP_ToIATA,
+                    FromDate = TEMP_ValidFrom,
+                    ToDate = TEMP_ValidTo,
+                    ArrivalTime = TEMP_ArrivalTime,
+                    DepartTime = TEMP_DepartTime,
+                    FlightAircraft = TEMP_Aircraftcode,
+                    FlightAirline = TEMP_Airline,
+                    FlightMonday = TEMP_FlightMonday,
+                    FlightTuesday = TEMP_FlightTuesday,
+                    FlightWednesday = TEMP_FlightWednesday,
+                    FlightThursday = TEMP_FlightThursday,
+                    FlightFriday = TEMP_FlightFriday,
+                    FlightSaterday = TEMP_FlightSaterday,
+                    FlightSunday = TEMP_FlightSunday,
+                    FlightNumber = TEMP_FlightNumber,
+                    FlightOperator = TEMP_FlightOperator,
+                    FlightDuration = TEMP_DurationTime.ToString().Replace("-", ""),
+                    FlightCodeShare = TEMP_FlightCodeShare,
+                    FlightNextDayArrival = TEMP_FlightNextDayArrival,
+                    FlightNextDays = TEMP_FlightNextDays
+                });
             }
+            // Write the list of objects to a file.
+            System.Xml.Serialization.XmlSerializer writer =
+            new System.Xml.Serialization.XmlSerializer(CIFLights.GetType());
+            string myDirOut = AppDomain.CurrentDomain.BaseDirectory + "\\output";
+            System.IO.Directory.CreateDirectory(myDirOut);
+            System.IO.StreamWriter file =
+               new System.IO.StreamWriter("output\\output.xml");
+
+            writer.Serialize(file, CIFLights);
+            file.Close();
+            Console.ReadKey();
 
         }
         
